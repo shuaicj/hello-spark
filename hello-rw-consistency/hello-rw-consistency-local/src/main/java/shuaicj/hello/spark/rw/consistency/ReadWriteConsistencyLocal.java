e46c45a056d168d7cd6412c4129c35b7d3518832
shuaicj <shuaicj@gmail.com>
2017-05-08 15:20:32 +0800

    hello-rw-consistency-local: md5 check

--- hello-rw-consistency/hello-rw-consistency-local/src/main/java/shuaicj/hello/spark/rw/consistency/ReadWriteConsistencyLocal.java
+++ hello-rw-consistency/hello-rw-consistency-local/src/main/java/shuaicj/hello/spark/rw/consistency/ReadWriteConsistencyLocal.java
@@ -1,17 +1,23 @@
 package shuaicj.hello.spark.rw.consistency;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.springframework.util.DigestUtils;
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Verify r/w consistency on Ceph local storage.
@@ -46,73 +52,91 @@ public class ReadWriteConsistencyLocal {
         while (!stop) {
             final long timestamp = System.currentTimeMillis();
 
-            Map<String, Long> writeResult = sc.parallelize(list, list.size())
-                    .map(i -> write(dir + "/" + timestamp + "-" + i, String.valueOf(i), size))
-                    .countByValue();
-            System.out.println(timestamp + " write: " + writeResult);
-            if (!writeResult.containsKey("ok") || writeResult.get("ok") != list.size()) {
+            List<FileInfo> writeResult = sc.parallelize(list, list.size())
+                    .flatMap(i -> write(dir + "/" + timestamp + "-" + i, size))
+                    .collect();
+            if (!writeResult.stream().allMatch(info -> info.getStatus().equals("ok"))) {
+                System.out.println(timestamp + " write: " + writeResult);
                 break;
             }
 
-            Map<String, Long> readwResult = sc.parallelize(list, list.size())
-                    .map(i -> read(dir + "/" + timestamp + "-" + i, String.valueOf(i), size))
+            Map<String, Long> readResult = sc.parallelize(writeResult, writeResult.size())
+                    .map(ReadWriteConsistencyLocal::read)
                     .countByValue();
-            System.out.println(timestamp + " readw: " + readwResult);
-            if (!readwResult.containsKey("ok") || readwResult.get("ok") != list.size()) {
+            if (!readResult.containsKey("ok") || readResult.get("ok") != writeResult.size()) {
+                System.out.println(timestamp + " read: " + readResult);
                 break;
             }
 
-            Map<String, Long> deleteResult = sc.parallelize(list, list.size())
-                    .map(i -> delete(dir + "/" + timestamp + "-" + i))
+            Map<String, Long> deleteResult = sc.parallelize(writeResult, writeResult.size())
+                    .map(ReadWriteConsistencyLocal::delete)
                     .countByValue();
-            System.out.println(timestamp + " delete: " + deleteResult);
-            if (!deleteResult.containsKey("ok") || deleteResult.get("ok") != list.size()) {
+            if (!deleteResult.containsKey("ok") || deleteResult.get("ok") != writeResult.size()) {
+                System.out.println(timestamp + " delete: " + deleteResult);
                 break;
             }
 
-            Map<String, Long> readdResult = sc.parallelize(list, list.size())
-                    .map(i -> read(dir + "/" + timestamp + "-" + i, String.valueOf(i), size))
+            Map<String, Long> existResult = sc.parallelize(writeResult, writeResult.size())
+                    .map(ReadWriteConsistencyLocal::exists)
                     .countByValue();
-            if (readdResult.containsKey("ok")) {
+            if (existResult.containsKey("ok")) {
+                System.out.println(timestamp + " exists: " + existResult);
                 break;
             }
         }
     }
 
-    private static String write(String file, String text, int size) {
-        try (Writer writer = new FileWriter(file)) {
-            int count = size / text.length();
-            for (int i = 0; i < count; i++) {
-                writer.write(text);
-            }
-        } catch (IOException e) {
-            return e.toString();
-        }
-        return "ok";
-    }
-
-    private static String read(String file, String text, int size) {
-        try (Reader reader = new FileReader(file)) {
-            int count = size / text.length();
-            char[] chars = text.toCharArray();
-            char[] buf = new char[chars.length];
-            for (int i = 0; i < count; i++) {
-                int num = reader.read(buf);
-                if (num != chars.length || !Arrays.equals(chars, buf)) {
-                    return "content not same";
+    private static Iterator<FileInfo> write(String prefix, int size) {
+        Random r = new Random();
+        byte[] buf = new byte[1024];
+        List<FileInfo> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            String file = prefix + "-" + i;
+            try (OutputStream out = new FileOutputStream(file)) {
+                for (int count = size; count > 0; count -= buf.length) {
+                    r.nextBytes(buf);
+                    out.write(buf, 0, count < buf.length ? count : buf.length);
                 }
+                out.flush();
+            } catch (IOException e) {
+                list.add(new FileInfo(e.toString(), file, ""));
+                continue;
             }
-        } catch (IOException e) {
-            return e.toString();
+            list.add(new FileInfo("ok", file, md5(file)));
         }
-        return "ok";
+        return list.iterator();
     }
 
-    private static String delete(String file) {
+    private static String read(FileInfo info) {
+        String actual = md5(info.getFile());
+        return info.getMd5().equals(actual) ? "ok" : "actual:" + actual + " " + info;
+    }
+
+    private static String delete(FileInfo info) {
         try {
-            return new File(file).delete() ? "ok" : "failed";
+            return new File(info.getFile()).delete() ? "ok" : "failed";
         } catch (SecurityException e) {
             return e.toString();
         }
+    }
+
+    private static String exists(FileInfo info) {
+        return new File(info.getFile()).exists() ? "ok" : "failed";
+    }
+
+    private static String md5(String file) {
+        try (InputStream in = new FileInputStream(file)) {
+            return DigestUtils.md5DigestAsHex(in);
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    @SuppressWarnings("serial")
+    @Getter
+    @AllArgsConstructor
+    @ToString(exclude = "status")
+    public static class FileInfo implements Serializable {
+        private final String status, file, md5;
     }
 }
