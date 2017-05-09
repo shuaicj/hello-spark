80c28256f2f811cad9171eecb708d0552bd25264
shuaicj <shuaicj@gmail.com>
2017-05-09 11:21:20 +0800

    hello-rw-consistency-local: read 3 times and pretty print

--- hello-rw-consistency/hello-rw-consistency-local/run.sh
+++ hello-rw-consistency/hello-rw-consistency-local/run.sh
@@ -4,13 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 spark-submit \
     --name ReadWriteConsistencyLocal \
     --class shuaicj.hello.spark.rw.consistency.ReadWriteConsistencyLocal \
-    --master spark://HOST:PORT \
+    --master spark://SPARK-HOST:SPARK-PORT \
     --executor-cores 1 \
     --executor-memory 1200M \
     --conf spark.shuffle.service.enabled=true \
     --conf spark.dynamicAllocation.enabled=true \
     --conf spark.dynamicAllocation.executorIdleTimeout=30s \
     $DIR/hello-rw-consistency-local-1.0.0.jar \
-    --dir DIR_READ_WRITE \
-    --num FILE_NUM \
-    --size FILE_SIZE
+    --dir /my/test/dir \
+    --num 36 \
+    --size 10000000
--- hello-rw-consistency/hello-rw-consistency-local/src/main/java/shuaicj/hello/spark/rw/consistency/ReadWriteConsistencyLocal.java
+++ hello-rw-consistency/hello-rw-consistency-local/src/main/java/shuaicj/hello/spark/rw/consistency/ReadWriteConsistencyLocal.java
@@ -14,10 +14,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 
 /**
  * Verify r/w consistency on Ceph local storage.
@@ -52,6 +54,7 @@ public class ReadWriteConsistencyLocal {
         while (!stop) {
             final long timestamp = System.currentTimeMillis();
 
+            // write files
             List<FileInfo> writeResult = sc.parallelize(list, list.size())
                     .flatMap(i -> write(dir + "/" + timestamp + "-" + i, size))
                     .collect();
@@ -60,14 +63,26 @@ public class ReadWriteConsistencyLocal {
                 break;
             }
 
-            Map<String, Long> readResult = sc.parallelize(writeResult, writeResult.size())
-                    .map(ReadWriteConsistencyLocal::read)
-                    .countByValue();
-            if (!readResult.containsKey("ok") || readResult.get("ok") != writeResult.size()) {
-                System.out.println(timestamp + " read: " + readResult);
+            // read for three times
+            boolean readFailed = false;
+            List<Map<String, Long>> readResults = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                Map<String, Long> readResult = sc.parallelize(writeResult, writeResult.size())
+                        .map(ReadWriteConsistencyLocal::read)
+                        .countByValue();
+                if (!readResult.containsKey("ok") || readResult.get("ok") != writeResult.size()) {
+                    readFailed = true;
+                }
+                readResults.add(readResult);
+            }
+            if (readFailed) {
+                for (Map<String, Long> readResult : readResults) {
+                    System.out.println(timestamp + " read:\n" + prettyMap(readResult));
+                }
                 break;
             }
 
+            // delete files
             Map<String, Long> deleteResult = sc.parallelize(writeResult, writeResult.size())
                     .map(ReadWriteConsistencyLocal::delete)
                     .countByValue();
@@ -76,6 +91,7 @@ public class ReadWriteConsistencyLocal {
                 break;
             }
 
+            // check existence
             Map<String, Long> existResult = sc.parallelize(writeResult, writeResult.size())
                     .map(ReadWriteConsistencyLocal::exists)
                     .countByValue();
@@ -86,30 +102,41 @@ public class ReadWriteConsistencyLocal {
         }
     }
 
+    /**
+     * Write two files with the same content for comparison.
+     * @param prefix two files will be named as ${prefix}-0 and ${prefix}-1
+     * @param size file size
+     * @return file info of two
+     */
     private static Iterator<FileInfo> write(String prefix, int size) {
         Random r = new Random();
         byte[] buf = new byte[1024];
-        List<FileInfo> list = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            String file = prefix + "-" + i;
-            try (OutputStream out = new FileOutputStream(file)) {
+        String file0 = prefix + "-0";
+        String file1 = prefix + "-1";
+        try (OutputStream out0 = new FileOutputStream(file0)) {
+            try (OutputStream out1 = new FileOutputStream(file1)) {
                 for (int count = size; count > 0; count -= buf.length) {
                     r.nextBytes(buf);
-                    out.write(buf, 0, count < buf.length ? count : buf.length);
+                    out0.write(buf, 0, count < buf.length ? count : buf.length);
+                    out1.write(buf, 0, count < buf.length ? count : buf.length);
                 }
-                out.flush();
-            } catch (IOException e) {
-                list.add(new FileInfo(e.toString(), file, ""));
-                continue;
+                out0.flush();
+                out1.flush();
             }
-            list.add(new FileInfo("ok", file, md5(file)));
+        } catch (IOException e) {
+            return Arrays.asList(new FileInfo(e.toString(), file0, ""), new FileInfo(e.toString(), file1, "")).iterator();
         }
-        return list.iterator();
+
+        String md50 = md5(file0);
+        String md51 = md5(file1);
+        String status = md50.equals(md51) ? "ok" : "copy not same";
+        return Arrays.asList(new FileInfo(status, file0, md50), new FileInfo(status, file1, md51)).iterator();
     }
 
     private static String read(FileInfo info) {
         String actual = md5(info.getFile());
-        return info.getMd5().equals(actual) ? "ok" : "actual:" + actual + " " + info;
+        return info.getMd5().equals(actual) ? "ok" :
+                "file:" + info.getFile() + " expect:" + info.getMd5() + " actual:" + actual;
     }
 
     private static String delete(FileInfo info) {
@@ -130,6 +157,12 @@ public class ReadWriteConsistencyLocal {
         } catch (Exception e) {
             return e.toString();
         }
+    }
+
+    private static String prettyMap(Map<String, Long> map) {
+        StringBuilder sb = new StringBuilder();
+        new TreeMap<>(map).forEach((k, v) -> sb.append(k).append(" n:").append(v).append('\n'));
+        return sb.toString();
     }
 
     @SuppressWarnings("serial")
